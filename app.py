7a894984dc90704d18c2231ea7f1c50ddb5b69bd
unknown <pauovictorpinheiro998663264@gmail.com>
2025-09-03 16:24:53 -0300

    Updating app nd models/  & create database

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from flask import Flask, render_template, jsonify
-from flask_sqlalchemy import SQLAlchemy
-
+from flask import Flask, render_template, jsonify, request
+from models.user import User
+from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "Your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-db = SQLAlchemy(app)
 
-@app.route('/Hello', methods=['GET'])
-def hello_world():
-    return 'hello world'
+from database import db
+Login_manager=LoginManager()
+db.init_app(app)
+Login_manager.init_app(app)
+#view login
+
+@app.route('/login', methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        #login
+        return jsonify({"message": "Autenticação realizada com sucesso"})
+    
+    return jsonify({"message": "Credenciais invalidas"}), 400
+
+if __name__ == "__main__":
+    app.run(debug=True)
