da82ec126448ca9350bf8dea86233dc844f7de14
Paulo Victor Pinheiro <pauovictorpinheiro998663264@gmail.com>
2025-09-06 02:31:53 -0300

    Adding perfil roles

--- models/user.py
+++ models/user.py
@@ -5,10 +5,12 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
-
-    def __init__(self, username=None, password=None):
+    role = db.Column(db.String(80), nullable=False, default='user')
+    def __init__(self, username=None, password=None, role='user'):
         self.username = username
         self.password = password
+        self.role = role
+
 
     def __repr__(self):
         return f'<User {self.username}>'
